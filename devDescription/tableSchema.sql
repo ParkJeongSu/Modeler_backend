bf92b518eebeb876103fc49f26db29ffcd43d83f
Park Jeong Su <pjs91_marrow@naver.com>
2022-07-03 16:56:09 +0900

    2022.07.03 modeler project add dev description add menu Controller

---
+++ devDescription/tableSchema.sql
@@ -0,0 +1,80 @@
+-- objectDefinition
+CREATE TABLE objectDefinition
+(
+    tableName VARCHAR2(40),
+    columnName VARCHAR2(40),
+    POSITION NUMBER(3)
+);
+
+ALTER TABLE objectDefinition ADD CONSTRAINT objectDefinition_pk PRIMARY KEY (tableName,columnName,POSITION);
+
+COMMENT ON
+TABLE objectDefinition IS '테이블정의';
+
+COMMENT ON
+COLUMN objectDefinition.tableName IS '테이블이름';
+
+COMMENT ON
+COLUMN objectDefinition.columnName IS '컬럼이름';
+
+COMMENT ON
+COLUMN objectDefinition.position IS '컬럼순서';
+
+
+-- userDefinition
+
+CREATE TABLE userDefinition
+(
+    userId VARCHAR2(40),
+    password VARCHAR2(40),
+    email VARCHAR2(40),
+    phoneNumber VARCHAR2(40),
+    level VARCHAR2(40), -- Admin,nomal 관리자, 일반유저
+    groupName VARCHAR2(40)
+);
+
+ALTER TABLE userDefinition ADD CONSTRAINT userDefinition_pk PRIMARY KEY (userId);
+
+COMMENT ON
+TABLE userDefinition IS '사용자정의';
+
+COMMENT ON
+COLUMN userDefinition.userId IS '사용자아이디';
+
+COMMENT ON
+COLUMN userDefinition.password IS '사용자비밀번호';
+
+COMMENT ON
+COLUMN userDefinition.email IS '사용자이메일';
+
+COMMENT ON
+COLUMN userDefinition.phoneNumber IS '사용자전화번호';
+
+COMMENT ON
+COLUMN userDefinition.level IS '사용자레벨';
+
+COMMENT ON
+COLUMN userDefinition.groupName IS '사용자그룹이름';
+
+-- menuDefinition
+
+CREATE TABLE menuDefinition
+(
+    menuName VARCHAR2(40),
+    position NUMBER(40),
+    superMenuName VARCHAR2(40)
+);
+
+ALTER TABLE menuDefinition ADD CONSTRAINT menuDefinition_pk PRIMARY KEY (menuName,position,superMenuName);
+
+COMMENT ON
+TABLE menuDefinition IS '메뉴정의';
+
+COMMENT ON
+COLUMN menuDefinition.menuName IS '메뉴이름';
+
+COMMENT ON
+COLUMN menuDefinition.position IS '메뉴순서';
+
+COMMENT ON
+COLUMN menuDefinition.superMenuName IS '상위메뉴이름';
--- src/main/java/com/parkjeongsu/modeler/controller/MenuDefinitionController.java
+++ src/main/java/com/parkjeongsu/modeler/controller/MenuDefinitionController.java
@@ -1,29 +1,37 @@
 package com.parkjeongsu.modeler.controller;
 
+import com.parkjeongsu.modeler.domain.MenuDefinition;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MenuDefinitionController {
 
-    @GetMapping("hello-api")
+    @GetMapping("users")
     @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name ){
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
-    static class Hello{
-        private String name;
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
+    public List<MenuDefinition> getUser(){
+        List<MenuDefinition> menuDefinitionList = new ArrayList<>();
+
+        MenuDefinition menuDefinition1 = new MenuDefinition();
+        menuDefinition1.setMenuName("test1");
+        menuDefinition1.setPosition(1L);
+        menuDefinition1.setSuperMenuName("test");
+
+        MenuDefinition menuDefinition2 = new MenuDefinition();
+        menuDefinition2.setMenuName("test2");
+        menuDefinition2.setPosition(1L);
+        menuDefinition2.setSuperMenuName("test2");
+
+        menuDefinitionList.add(menuDefinition1);
+        menuDefinitionList.add(menuDefinition2);
+
+        return menuDefinitionList;
     }
 
 }
